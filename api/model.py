1c7e65233bd8d2155b38a3abd82ea26f2720be5b
dummyVariable <officialsanjivee@gmail.com>
2020-09-10 17:27:46 +0530

    added read attribute for model

--- api/model.py
+++ api/model.py
@@ -1,6 +1,28 @@
+from typing import Union
 
 from api import rc as client
 
 class CrudModel:
 
-    pass
+    @staticmethod
+    def read_all() -> Union[dict, None]:
+        keys = client.keys('*')
+        
+        if not keys:
+            return None
+
+        data = []
+        for key in keys:
+            data.append(client.get(key))
+
+        return data
+    
+    @staticmethod
+    def read(id: int) -> Union[dict, None]:
+
+        data = client.get(id)
+
+        if data:
+            return data
+
+        return None
--- tests/test_model.py
+++ tests/test_model.py
@@ -3,6 +3,5 @@ from api.model import CrudModel as db
 
 def test_for_emptyness():
 
-    assert db.read() == None
+    assert db.read_all() == None
     assert db.read(1) == None
-    
