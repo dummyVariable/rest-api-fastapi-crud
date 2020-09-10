61dc54b5f657f515e869022308ffe5dd860f017c
dummyVariable <officialsanjivee@gmail.com>
2020-09-10 19:29:46 +0530

    added tests for create

--- tests/test_api.py
+++ tests/test_api.py
@@ -15,3 +15,20 @@ def test_for_emptyness():
     
     resp2 = req.get(URL + '/1')
     assert resp2.json() == {'message' : 'item not found'}
+
+def test_for_create():
+
+    resp1 = req.post(URL, data={'message' : 'Im batman'})
+    assert resp1.json() == {'message' : 'created'}
+    
+    resp2 = req.get(URL + '/1')
+    assert resp2.json() == {'message' : 'Im batman'}
+    
+    resp3 = req.post(URL, data={'message' : 'Michael Scorn'})
+    assert resp3.json() == {'message' : 'created'}
+
+    resp4 = req.get(URL + '/2')
+    assert resp4.json() == {'message' : 'Michael Scorn'}
+
+    resp5 = req.get(URL)
+    assert len(resp5.json()) == 2
